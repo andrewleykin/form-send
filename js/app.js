6489a6840b8b08e6971f5be42a4ecbc7b3d0ac39
Andrew Leykin <andrewleykin@mail.ru>
2018-11-26 17:34:27 +0300

    change form data

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 (function(){
   $('#form').submit(function(e){
     e.preventDefault();
-    console.log($(this).serialize());
+    var fd = new FormData(document.forms.form);
+    console.log(fd);
     $.ajax({
       method: 'POST',
       url: "https://webdev-api.loftschool.com/sendmail",
-      data: $(this).serialize()
-    }).done(function() {
-      $( this ).addClass( "done" );
-    });
+      data: fd,
+      processData: false,
+      contentType: false,
+      success: function(data){
+        alert(data);
+      }
+    })
   });
 })();
